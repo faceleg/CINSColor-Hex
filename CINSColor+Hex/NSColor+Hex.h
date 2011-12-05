9cf797692dbd00e1f42145703831406f7776090b
Michael Robinson <mike@pagesofinterest.net>
2011-12-06 00:28:28 +1300

    Added NSColor+Hex category

---
+++ CINSColor+Hex/NSColor+Hex.h
@@ -0,0 +1,17 @@
+//
+//  NSColor+Hex.h
+//  CINSColor+Hex
+//
+//  Created by Michael Robinson on 4/12/11.
+//  Copyright 2011 Code of Interest. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface NSColor (Hex)
+
++ (NSColor *) colorWithHex:(NSString *)hexColor;
+
+- (NSString *) hexColor;
+
+@end
